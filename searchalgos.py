65271376f2fbc1e529df5788bd8ba836e3a7207f
mxhofer <maximilian.hofer.14@ucl.ac.uk>
2015-12-15 23:55:55 +0000

    Update searchalgos.py

--- searchalgos.py
+++ searchalgos.py
@@ -15,6 +15,9 @@ def insertionSort (A, direction = "up"):  # first-try version
                 i = i-1
     return A
     
+# >>>insertionSort([1,3,2,6,5])
+
+    
 def insertionSort (A):  # improved version
     for j in range(1,len(A)):
         key = A[j]
@@ -24,6 +27,9 @@ def insertionSort (A):  # improved version
             i = i-1
         A[i+1] = key
     return A
+    
+# >>>insertionSort([1,3,2,6,5])
+
 
 def insertionSort (A) :  # improved version with print() statements to understand better
     print ("list to be sorted = ", A)
@@ -40,9 +46,12 @@ def insertionSort (A) :  # improved version with print() statements to understan
     print ("sorted list = ", A)
     return A
 
-#bubble sort algorithm
-#you compare two adjacent numbers and swap them if not ascending
-#this is a polynomial time algorithm, because it got a square in it due to the two loops. Quite inefficient.
+# >>>insertionSort([1,3,2,6,5])
+
+
+# bubble sort algorithm
+# you compare two adjacent numbers and swap them if not ascending
+# this is a polynomial time algorithm, because it got a square in it due to the two loops. Quite inefficient.
 def bubbleSort(A):
     for b in range(len(A)-1):
         for i in range(len(A)-1-b):
@@ -53,16 +62,26 @@ def bubbleSort(A):
         if not(swap):
             break
     return A
+    
+# >>>bubbleSort([1,7,3,2,4])
+
 
 def search(findItem,list):
-    for counter,item in enumerate(list): #enumarte creates format of [counter, item]
+    for counter,item in enumerate(list):  # enumarte creates format of [counter, item]
         if item==findItem:
             return counter
     else:
         return "n not in list"
+        
+# >>>search(5, [1,6,5,8,6,1,2])
+# note that all lists start at index 0
 
-#easier method using Python's built in .index method
+
+# easier method using Python's built in .index method
 def easysearch(nlist, n):
     if n in nlist:
         return nlist.index(n)
     else: return "change n"
+
+# >>>easysearch([1,6,3,7,6,3],3)
+# note that all lists start at index 0
