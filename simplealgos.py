175a4bb61a72b4228b5f2eafebbbadc4ab8de00e
mxhofer <maximilian.hofer.14@ucl.ac.uk>
2015-12-15 23:46:46 +0000

    Update simplealgos.py

--- simplealgos.py
+++ simplealgos.py
@@ -1,3 +1,5 @@
+__author__ = 'maxhofer'
+
 # function that counts the number of vowels in a string
 def countvowels(s):
     count = 0
@@ -28,3 +30,52 @@ def isPalindrome(s):
 		return True
 	else:
 		return s[0]==s[-1] and isPalindrome(s[1:-1])  # s[-1] is the last character
+		
+
+def inputNumbers(n):
+    answer = []
+    i = 1
+    while i <= n:
+        x = int(input("Enter a number: "))
+        answer.append(x)
+        i += 1
+    return answer  # don't use print statements as part of your function
+
+
+def integrate(x, start, insert, end):
+    answer = x**2
+    newlist = [start, insert, end]
+    newlist[1] = answer
+    return newlist
+
+def integrate(start, end, dx):
+    x = start
+    answer = 0
+    while x < end:
+        area = dx * x**2
+        answer += area
+        x += dx
+    return answer
+
+def squareRoot(x): #finds the square root manually and stops when error < 0.01
+    guess = 0
+    dx = 0.01
+    while abs(guess**2 - x) > 0.001:
+        guess += dx
+        if guess > x: #Just guess > x because it's the easiest thing to do
+            return None
+    return guess
+
+
+def squareRoot(x, error):  # bisection search is super efficient
+    low = 0
+    high = x
+    guess = (high+low)/2
+    counter = 0
+    while abs(guess**2 - x) > error:  # never goes into an infinite loop!!
+        if guess**2 < x:
+            low = guess
+        else: high = guess
+        guess = (low+high)/2
+        counter += 1
+    return guess, counter
