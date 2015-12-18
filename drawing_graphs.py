2ce3df43e280355cd25966847ce9a3213f12c660
mxhofer <maximilian.hofer.14@ucl.ac.uk>
2015-12-18 13:18:20 +0000

    Create drawing_graphs.py

---
+++ drawing_graphs.py
@@ -0,0 +1,46 @@
+__author__ = 'andrewwhiter'
+# comments = Max
+
+import csv
+def getEdgeListFromCSVfile(filename):
+    ''' returns a list of graph edges eg [("A","B"),("A","C"),("B","E")] '''
+    with open(filename, newline='') as f:
+        return list(csv.reader(f))
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def draw_graph(graph, showLabels = True):
+
+    # extract nodes from graph
+    nodes = set([n1 for n1, n2 in graph] + [n2 for n1, n2 in graph])
+
+    # create network graph
+    G=nx.Graph()
+
+    # add nodes
+    for node in nodes:
+        G.add_node(node)
+
+    # add edges
+    for edge in graph:
+        G.add_edge(edge[0], edge[1])
+
+    # draw graph 1
+    pos = nx.shell_layout(G)
+    nx.draw(G, pos)
+    plt.figure()
+
+    # draw graph 2
+    # labels are now on top of the nodes
+    nx.draw_random(G)
+    plt.figure()
+    nx.draw_spring(G, node_size=100, with_labels=showLabels, font_size=16, edge_color="grey", width=0.5)
+
+    # show and save graph
+    plt.savefig("fig1.png")
+    plt.show()
+
+# draw example
+graph = getEdgeListFromCSVfile("/Users/maximilianhofer/PycharmProjects/LecturesSeminars/FacebookEdgeData.csv")
+draw_graph(graph, showLabels=False)
